b67e11e21d39ad2020ba2625646651b496704638
presiga <presigadurangoesteban@gmail.com>
2025-03-26 10:58:28 -0500

    Initial commit: ejercicios subidos

---
+++ 01/ParImpar/Program.cs
@@ -0,0 +1,10 @@
+﻿console.WriteLine("ingrese un numero");
+var numero = console.ReadLine();
+if (numero / 2 == 0)
+{
+    console.WriteLine("el numero es par");
+}
+else
+{
+    console.WriteLine("el numero es impar");
+}
