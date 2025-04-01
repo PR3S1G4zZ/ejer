fae384fa7fe7e9a92c6610d94d20bf4b9ec274ea
PR3S1G4zZ <153407926+PR3S1G4zZ@users.noreply.github.com>
2025-04-01 06:43:14 -0500

    Update Program.cs

--- 01/ParImpar/Program.cs
+++ 01/ParImpar/Program.cs
@@ -1,10 +1,27 @@
-﻿console.WriteLine("ingrese un numero");
-var numero = console.ReadLine();
-if (numero / 2 == 0)
-{
-    console.WriteLine("el numero es par");
-}
-else
-{
-    console.WriteLine("el numero es impar");
-}
+var i = 1;
+do { 
+
+Console.Write($"Ingrese el numero:{i} un numero Entero:");
+var numString = (Console.ReadLine());
+var num = int.Parse( numString );
+
+
+    if (int.TryParse(numString, out int numInt))
+    {
+        if (numInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numInt} es par!!");
+
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numInt} es impar!!");
+        }
+    }
+    else 
+    {
+        Console.WriteLine("El valor '" + $"{numString}" + "' no es un numero Entero !!");
+
+    }
+    i++;
+}while (i <= 3);
